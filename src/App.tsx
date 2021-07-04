695cbd916f74502c729488428ab935ae89fef656
Aantoni018 <antoniocrf.info@gmail.com>
2021-07-04 17:21:14 -0300

    Aula 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import { Home } from './pages/Home';
 import { NewRoom } from "./pages/NewRoom";
+import {Room} from './pages/Room';
 
 import { AuthContextProvider } from './contexts/AuthContext';
 
@@ -10,8 +10,11 @@ function App() {
   return (
     <BrowserRouter>
       <AuthContextProvider>
+        <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/rooms/new" component={NewRoom}/>
+        <Route path="/rooms/:id" component={Room}/>
+        </Switch>
       </AuthContextProvider>
     </BrowserRouter>
     
